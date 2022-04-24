da8449dfd4e6aeaaebcbe4e852f78c3fae50a27d
rathodashwini990 <rathodashwini990.com>
2022-04-25 02:09:46 +0530

    created login and signup pages

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import { Home } from './Components/Home/Home';
 import CarouselApp from './Components/Home/CarouselApp';
 import Login from './Components/Navbar/Login';
 import Signup from './Components/Navbar/Signup';
+import SignInOutContainer from './Components/Navbar/SignInOutContainer';
 
 function App() {
   
 
   return (
     <div className="App">
-     <Navbar />
+     {/* <Navbar />
      <Home />
      <CarouselApp />
      <Footer />
      <Login />
-     <Signup />
+     <Signup /> */}
+     <SignInOutContainer />
     </div>
   );
 }
